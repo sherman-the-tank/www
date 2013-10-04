e9bb6c4cf974f254194acf684c039be3ad16c01e
Ubuntu <ubuntu@ip-172-31-43-86.us-west-2.compute.internal>
2013-10-04 02:41:11 +0000

    Initiated the login page

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo '<b>Hello World from PHP</b>';
+
+?>
---
+++ login.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+  <title>Please login to Koo...</title>
+</head>
+
+<body>
+  <form>
+    <table>
+      <tr>
+        <td colspa='2'>Please enter the username and password...</td>
+      </tr>
+      <tr>
+        <td>Username:</td>
+        <td><input /></td>
+      </tr>
+      <tr>
+        <td>Password:</td>
+        <td><input type='password' /></td>
+      </tr>
+      <tr>
+        <td colspan='2'>
+          <table width='100%'>
+            <tr>
+              <td width='50%' align='center'><button type='submit'>Login</button></td>
+              <td align='center'><a href='#'>Sign Up</a></td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </form>
+</body>
+</html>
