c1eab1e95cad7d9701c41e01f982e9aa6c1fb007
Ubuntu <ubuntu@ip-172-31-43-86.us-west-2.compute.internal>
2014-01-26 00:40:11 +0000

    Added load_reservation.php

---
+++ load_bu_menu.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+  <title>Load all menu items for a given business unit...</title>
+</head>
+
+<?php
+  $buid = $_REQUEST['bu_id'];
+  if (!$buid) {
+    echo 'No business unit specified';
+    return;
+  }
+
+  $stmt = 'SELECT * FROM bu_item WHERE bu_id=' . $buid;
+  $mysql_client = mysqli_connect('koodemo.cwmhshxpuljc.us-west-2.rds.amazonaws.com',
+                                 'koomaster',
+                                 'koopassword',
+                                 'koodb');
+  if ($mysql_client->connect_errno) {
+    echo 'Failed to connect to Mysql: ' . $mysql_client->error
+         . ' (' . $mysql_client->connect_errno . ')';
+    return;
+  } else {
+    $result = $mysql_client->query($stmt);
+    if ($result) {
+      $rows = $result->fetch_all(MYSQLI_ASSOC);
+      echo json_encode($rows);
+      $result->free();
+      $mysql_client->close();
+    } else {
+      echo 'Failed to load all menu items: ' . $mysql_client->error;
+      $mysql_client->close();
+      return;
+    }
+  }
+?>
--- load_reservation.php
+++ load_reservation.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <title>Load all items for a given  business unit...</title>
+  <title>Load service reservations on a specific day for a given business unit...</title>
 </head>
 
 <?php
@@ -10,7 +10,21 @@
     return;
   }
 
-  $stmt = 'SELECT * FROM bu_item WHERE bu_id=' . $buid;
+  // Start date. If not specified, will be the current time
+  $start = $_REQUEST['start_time'];
+  if (!$start) {
+    // Not specified, use the current time
+    $start = date('Y-m-d H:i:s');
+  }
+
+  // End time. If not specified, will retrieve all reservations starting from start time
+  $end = $_REQUEST['end_time'];
+  if (!$end) {
+    $end = '9999-12-31 23:59:59';
+  }
+
+  $stmt = 'SELECT * FROM reservation WHERE bu_id=' . $buid
+          . ' AND reserved_time>="' . $start . '" AND reserved_time<"' . $end .'"';
   $mysql_client = mysqli_connect('koodemo.cwmhshxpuljc.us-west-2.rds.amazonaws.com',
                                  'koomaster',
                                  'koopassword',
@@ -27,7 +41,7 @@
       $result->free();
       $mysql_client->close();
     } else {
-      echo 'Failed to load all business units: ' . $mysql_client->error;
+      echo 'Failed to load reservations: ' . $mysql_client->error;
       $mysql_client->close();
       return;
     }
